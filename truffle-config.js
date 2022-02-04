07134c2d10ba9088bef211cd1607c84198e89a1d
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-05 01:29:16 +0900

    config: modify network APIs

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const path = require("path");
 
 module.exports = {
@@ -8,7 +9,20 @@ module.exports = {
     develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*",
+      gasLimit: "0x8000000",
+      gas: 0xfffffffff
+    },
+    ropsten:{
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: process.env.MNENOMIC
+        },
+        providerOrUrl: "https://ethereum-ropsten-rpc.allthatnode.com/" + process.env.DSRV_API_KEY
+      }),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 10000000000
     }
   },
   compilers: {
