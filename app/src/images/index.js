d9cb251b905976d37dd9de8c330ca75b887ff884
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-05 01:28:06 +0900

    chore: add logo images

---
+++ app/src/images/index.js
@@ -0,0 +1,7 @@
+import issue from './issue.png'
+import token from './token.png'
+import logo from './logo.png'
+
+export const Issue = issue;
+export const Token = token;
+export const Logo = logo;
