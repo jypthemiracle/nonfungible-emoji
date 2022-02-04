a4af3c3d3e277739327fd1d2a5c419dd3b28ef0a
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-05 01:53:48 +0900

    feat: add reducer for onEmojiChange

---
+++ app/src/layouts/issue/issueContainer.js
@@ -0,0 +1,24 @@
+import { drizzleConnect } from "@drizzle/react-plugin";
+import Issue from './issue'
+import { emojiChangeAction } from "../../actions/customAction";
+
+const mapStateToProps = (state) => (
+    {
+        emoji: state.customReducer.emoji,
+        accounts: state.accounts,
+        drizzleStatus: state.drizzleStatus,
+        DeedToken: state.contracts.DeedToken
+    }
+);
+
+const mapDispatchToProps = (dispatch) => (
+    {
+        onEmojiChange: (params) => {
+            dispatch(emojiChangeAction(params))
+        }
+    }
+)
+
+const IssueContainer = drizzleConnect(Issue, mapStateToProps, mapDispatchToProps);
+
+export default IssueContainer;
